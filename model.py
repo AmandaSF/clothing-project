2701676f5378fa9170ff8fcd1fff750e052c0759
Amanda Fuhrmann <amandafuhrmann@gmail.com>
2015-08-04 15:27:43 -0700

    added repr to model.py

--- model.py
+++ model.py
@@ -12,6 +12,11 @@ class User(db.Model):
 	user_id = db.Column(db.Integer, autoincrement=True, nullable=False)
 	email = db.Column(db.String(50), nullable=False)
 
+	def __repr__(self):
+		"""Represent info about user"""
+
+		return "<User user_id=%d email=%s>" % (self.user_id, self.email)
+
 
 class Share(db.Model):
 
@@ -21,10 +26,15 @@ class Share(db.Model):
 	s_size = db.Column(db.Integer, nullable=False)
 	s_style = db.Column(db.String(6), nullable=False)
 	s_item_type = db.Column(db.String(20), nullable=False)
+	s_active = db.Column(db.Boolean, unique=False, default=True)
 	user_email = db.Column(db.String(50), ForeignKey('users.email'
 		), nullable=False)
 
+	def __repr__(self):
+		"""Represent info from Share table"""
 
+		return "<Share share_id=%d s_item_type=%s user_email=%s>" % (
+			self.share_id, self.s_item_type, self.user_email)
 
 class Wish(db.Model):
 
@@ -34,11 +44,15 @@ class Wish(db.Model):
 	w_size = db.Column(db.Integer, nullable=False)
 	w_style = db.Column(db.String(6), nullable=False)
 	w_item_type = db.Column(db.String(20), nullable=False)
+	w_active = db.Column(db.Boolean, unique=False, default=True)
 	user_email = db.Column(db.String(50), ForeignKey('users.email'
 		), nullable=False)
 
-#needs repr!!!
+	def __repr__(self):
+		"""Represent info from Wish table"""
 
+		return "<Wish wish_id=%d w_item_type=%s user_email=%s>" % (
+			self.wish_id, self.w_item_type, self.user_email)
 
 #Helper functions 
 
