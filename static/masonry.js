c2aab0add6699599fa231a1c17b1e8fea1db70cc
amandasf <amandafuhrmann@gmail.com>
2015-09-01 17:22:03 -0700

    added mailto links

--- static/masonry.js
+++ static/masonry.js
@@ -1,24 +1,3 @@
-// var $display = $('.display').masonry({
-//   // options
-//   itemSelector: '.item',
-//   columnWidth: '.item',
-//   percentPosition: true
-// });
-
-
-// // layout Masonry after each image loads
-// $display.imagesLoaded().progress( function() {
-//   $display.masonry('layout');
-// });
-
-
-// ( function( $ ) {
-// var $container = $('.display');
-// //layout Masonry again after all images have loaded
-// $container.imagesLoaded( function() {
-// $container.masonry();
-// });})( jQuery );
-
 $('.display').imagesLoaded()
   .always( function( instance ) {
 
